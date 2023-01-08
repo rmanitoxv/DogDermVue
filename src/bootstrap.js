2c41a056c7714110d9dd4984ef47c939bbbac567
Russel Manito <manito.russel@gmail.com>
2023-01-09 01:50:45 +0800

    Test Backend

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,7 +12,7 @@ import 'bootstrap';
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.baseURL = 'http://13.250.100.240:8000';
+window.axios.defaults.baseURL = 'https://13.250.100.240:8000';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
