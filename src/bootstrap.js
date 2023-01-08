5a0c0c37a89ca8a2d1d6cc68781c3c22eca8fab1
Russel Manito <manito.russel@gmail.com>
2023-01-09 01:48:32 +0800

    Backend Update

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,7 +12,7 @@ import 'bootstrap';
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.baseURL = 'http://127.0.0.1:8000';
+window.axios.defaults.baseURL = 'http://13.250.100.240:8000';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
