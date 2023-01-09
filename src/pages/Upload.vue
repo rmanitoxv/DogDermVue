d1361e262023ce0f244168d45e12098a5688d61d
Jennah <108987116+beninaaa@users.noreply.github.com>
2023-01-09 21:42:18 +0800

    Update Upload.vue

--- src/pages/Upload.vue
+++ src/pages/Upload.vue
@@ -3,6 +3,11 @@
         <p class="text-2xl lg:text-[2.5rem] text-second font-semibold"> DOG SKIN DISEASE DETECTION </p>
         <p class="text-sm lg:text-md font-medium py-2 text-sixth"><br>Upload a close-up picture of the affected area of your dog.</p>
         <form class="align-self-center" @submit.prevent="afterComplete(file)">
+                <div class="my-3 p-3 border-2 rounded-3xl border-first bg-[#FFEFE1]">
+                    <p class="amiko text-first text-sm font-semibold">
+                            <b>NOTE:</b>
+                    </p>
+                </div>
             <div class="flex flex-col justify-center items-center">
                 <div v-if="canvas || video">
                     <div
