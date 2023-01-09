2abdfff01b9b93abd8c998702746f697aceb3369
Jennah <108987116+beninaaa@users.noreply.github.com>
2023-01-09 21:58:44 +0800

    Added note for limitation in skin diseases

--- src/pages/Upload.vue
+++ src/pages/Upload.vue
@@ -4,12 +4,6 @@
         <p class="text-sm lg:text-md font-medium py-2 text-sixth"><br>Upload a close-up picture of the affected area of your dog.</p>
         <form class="align-self-center" @submit.prevent="afterComplete(file)">
             <div class="flex flex-col justify-center items-center">
-                <div class="my-3 p-3 border-2 rounded-3xl border-first bg-[#FFEFE1] flex w-80 lg:w-[30.5rem] break-all items-center justify-center">
-                    <p class="amiko text-first text-sm font-semibold">
-                            <b>NOTE: Please keep in mind that this is still a beta version that can only support 10 dog skin diseases which are:
-                            <br> Ringworms, MRSA, Sarcoptic Mange, Flea Infestation, Tickborne Disease, Harvest Mites, Yeast Infections, Hot Spot, Folliculitis, and Carbuncles</b>
-                    </p>
-                </div>
                 <div v-if="canvas || video">
                     <div
                         class="flex w-80 lg:w-[30.5rem] h-[3rem] bg-first items-center justify-center mt-[1.75rem] mb-[2.5rem] rounded-2xl">
@@ -63,6 +57,23 @@
                 <div v-else class="flex justify-between w-80 lg:w-[24.5rem]">
                     <button type="button" class="w-[7.5rem] text-first border-[.15rem] border-first py-[.5rem] ml-[1rem] mr-[1rem] rounded-3xl mt-[2.5rem] text-md" @click="useCamera">Use Camera</button>
                     <button :class="submitClass" :disabled="saving"> {{ status }} </button>
+                </div>
+                <div class="my-3 p-3 border-2 rounded-3xl border-first bg-[#FFEFE1] flex w-80 lg:w-[30.5rem] break-all items-center justify-center">
+                    <p class="amiko text-first text-sm font-semibold">
+                            <b>NOTE: Please keep in mind that this is still a beta version that can only support 10 dog skin diseases which are:
+                            <br><br><ol>
+                                <li>Ringworms</li>
+                                <li>MRSA</li>
+                                <li>Sarcoptic Mange</li>
+                                <li>Flea Infestation</li>
+                                <li>Tickborne Disease</li>
+                                <li>Harvest Mites</li>
+                                <li>Yeast Infections</li>
+                                <li>Hot Spot</li>
+                                <li>Folliculitis</li>
+                                <li>Carbuncles</li>
+                            </ol></b>
+                    </p>
                 </div>
             </div>
         </form>
