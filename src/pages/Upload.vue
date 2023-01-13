ce041a4d74683af8cdea8cd6c4558bee3da59f90
Russel Manito <73598187+rmanitoxv@users.noreply.github.com>
2023-01-14 03:43:58 +0800

    Update Upload.vue

--- src/pages/Upload.vue
+++ src/pages/Upload.vue
@@ -52,7 +52,7 @@
                     </label>
                 </div>
                 <canvas ref="canvas" width="500" height="400"  class="hidden object-cover w-[24.5rem] h-[24.5rem] rounded-[1.5rem]"></canvas>                        
-                <input type="file" accept=".jpeg,.jpg,.png,.svg" class="hidden" id="upload" @change="getFileName(e)" />
+                <input type="file" accept="image/*" class="hidden" id="upload" @change="getFileName(e)" />
                 <div v-if="video" class="flex justify-between w-80 lg:w-[24.5rem] !mb-5">
                     <button type="button" class="w-[7.5rem] text-first border-[.15rem] border-first py-[.5rem] rounded-3xl mt-[2.5rem] text-lg" @click="useCamera">Cancel</button>
                     <button type="button" class="mt-[2.25rem] w-[7.25rem] py-[1rem] rounded-[2.75rem] bg-first text-white align-self-center" @click="captureImage">Capture</button>
