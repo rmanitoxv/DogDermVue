9616909b52a427751affa0866435a9dbb9fa7eac
Russel Manito <manito.russel@gmail.com>
2023-01-18 10:46:03 +0800

    Changed Firebase Storage

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from 'firebase/app';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnxPq9o8ElSjkrkTRjuhw77gHLP-e14cc",
-  authDomain: "dogderma-b5fbc.firebaseapp.com",
-  projectId: "dogderma-b5fbc",
-  storageBucket: "dogderma-b5fbc.appspot.com",
-  messagingSenderId: "340014456298",
-  appId: "1:340014456298:web:eb7c16c44e1764bb666693",
-  measurementId: "G-60V4SWH9PN"
+  apiKey: "AIzaSyAt4mdXXv5vuTrhoXGi3TQifDVpy0yTGUw",
+  authDomain: "dogderma-999d1.firebaseapp.com",
+  projectId: "dogderma-999d1",
+  storageBucket: "dogderma-999d1.appspot.com",
+  messagingSenderId: "932251037297",
+  appId: "1:932251037297:web:b7191b6eabe4e2071335d0",
+  measurementId: "G-0RRXYQSFB1"
 };
 
 export default firebaseConfig
